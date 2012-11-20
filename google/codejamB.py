ad9f204c90026c7857d311fe88d699cc31681bdd
Noah Swartz <swartzcr@gmail.com>
2012-11-20 16:02:28 -0500

    added a subdirectory for google

---
+++ google/codejamB.py
@@ -0,0 +1,17 @@
+#from: http://code.google.com/codejam/contest/351101/dashboard#s=p1
+
+def main():
+    filename = raw_input("enter a file name ")
+    with open(filename) as f:
+        data = f.readlines()
+    out = ""
+    for idx,line in enumerate(data[1:]):
+        words = line.split()
+        words.reverse()
+        y = " ".join(words)
+        out+="Case #"+str(idx+1)+": "+y+"\n"
+    with open(filename.split(".")[0]+".out", 'w') as fi:
+        fi.write(out)
+
+if __name__ == "__main__":
+    main()
